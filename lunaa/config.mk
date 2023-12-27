3a2bb7a123ac85594ec640c6a10364a2f2b46fb9
Quantum <14228163-CyberDroid23@users.noreply.gitlab.com>
2023-12-27 12:39:51 +0530

    lunaa: F.16: Import FW

---
+++ lunaa/config.mk
@@ -0,0 +1,26 @@
+FIRMWARE_IMAGES := \
+    abl \
+    aop \
+    bluetooth \
+    cpucp \
+    devcfg \
+    dsp \
+    engineering_cdt \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    multiimgoem \
+    oplus_sec \
+    oplusstanvbk \
+    qupfw \
+    qweslicstore \
+    shrm \
+    splash \
+    tz \
+    uefisecapp \
+    xbl_config \
+    xbl
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
